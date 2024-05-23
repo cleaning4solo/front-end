3740ff3d4cbc416b091f17462e7244932da00ca1
eunoiamo <miftahuda701@gmail.com>
2024-05-23 12:52:31 +0700

    install dependencies bootstrap

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,14 @@
 import 'regenerator-runtime';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import AOS from 'aos';
+import GLightbox from 'glightbox';
+import Swiper from 'swiper';
+import '../public/css/style.css';
+
+// Initialize libraries if necessary
+AOS.init();
+const lightbox = GLightbox();
+const swiper = new Swiper('.swiper-container');
  
 console.log('Hello Coders!');
