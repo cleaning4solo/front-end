6ea2b84d785d28ccd8c0c000cb84aba480311269
LabQii <sisteminformasi2006@gmail.com>
2024-05-24 23:52:46 +0700

    add navbar toggle

---
+++ src/scripts/components/mobile-nav.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+    "use strict";
+  
+    const mobileNavShow = document.querySelector(".mobile-nav-show");
+    const mobileNavHide = document.querySelector(".mobile-nav-hide");
+  
+    document.querySelectorAll(".mobile-nav-toggle").forEach((el) => {
+      el.addEventListener("click", function (event) {
+        event.preventDefault();
+        mobileNavToogle();
+      });
+    });
+  
+    function mobileNavToogle() {
+      document.querySelector("body").classList.toggle("mobile-nav-active");
+      mobileNavShow.classList.toggle("d-none");
+      mobileNavHide.classList.toggle("d-none");
+    }
+  
+    document.querySelectorAll("#navbar a").forEach((navbarlink) => {
+      if (navbarlink.getAttribute("href").startsWith("#")) {
+        navbarlink.addEventListener("click", (event) => {
+          event.preventDefault();
+          const targetId = navbarlink.getAttribute("href").substr(1);
+          const targetElement = document.getElementById(targetId);
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth" });
+            // Tutup mobile nav jika sedang aktif
+            if (document.querySelector(".mobile-nav-active")) {
+              mobileNavToogle();
+            }
+          }
+        });
+      }
+    });
+  
+    const navDropdowns = document.querySelectorAll(".navbar .dropdown > a");
+  
+    navDropdowns.forEach((el) => {
+      el.addEventListener("click", function (event) {
+        if (document.querySelector(".mobile-nav-active")) {
+          event.preventDefault();
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("dropdown-active");
+  
+          let dropDownIndicator = this.querySelector(".dropdown-indicator");
+          dropDownIndicator.classList.toggle("bi-chevron-up");
+          dropDownIndicator.classList.toggle("bi-chevron-down");
+        }
+      });
+    });
+  });
+  
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,40 +1,41 @@
-import 'regenerator-runtime';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../public/css/style.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Swiper from 'swiper';
-import App from './views/app';
-import './components/dark-mode';
-import navbarScroled from './components/navbar-scroled';
+import "regenerator-runtime";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../public/css/style.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Swiper from "swiper";
+import App from "./views/app";
+import "./components/dark-mode";
+import navbarScroled from "./components/navbar-scroled";
+import "./components/mobile-nav";
 
 AOS.init();
 
-const swiper = new Swiper('.swiper-container');
+const swiper = new Swiper(".swiper-container");
 
 const app = new App({
-  content: document.querySelector('#main-content'),
+  content: document.querySelector("#main-content"),
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   app.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   app.renderPage();
 });
-window.addEventListener('load', () => {
-  const preloader = document.getElementById('preloader');
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
   if (preloader) {
     preloader.remove();
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const storedMode = localStorage.getItem('darkMode');
-  if (storedMode === 'true') {
-    document.body.classList.add('dark-mode');
+document.addEventListener("DOMContentLoaded", () => {
+  const storedMode = localStorage.getItem("darkMode");
+  if (storedMode === "true") {
+    document.body.classList.add("dark-mode");
   }
   navbarScroled();
 });
