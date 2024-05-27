1ef479cd6b105769d19ad89b582d3cefc1007ef9
eunoiamo <miftahuda701@gmail.com>
2024-05-27 11:29:15 +0700

    Add purecounter

--- src/scripts/views/components/component-creator.js
+++ src/scripts/views/components/component-creator.js
@@ -3,7 +3,7 @@ const createHomePageComponent = () => `
 <div class="container position-relative">
   <div class="row gy-5">
     <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
-      <h2>Mari jaga Solo kita bersama <span>Sclean</span></h2>
+      <h2>Mari Jaga Solo Kita Bersama <span>Cleaning4Solo</span></h2>
       <p>Menjadi wadah generasi muda yang memiliki wawasan dan kepedulian terhadap kelestarian lingkungan di wilayah Karesidenan Surakarta.</p>
       <div class="d-flex justify-content-center justify-content-lg-start">
         <a href="#about" class="btn-get-started">Tentang Kami</a>
@@ -16,6 +16,44 @@ const createHomePageComponent = () => `
   </div>
 </div>
 
-`;
+`;  
 
-export { createHomePageComponent };
+const CreateCounterComponent = () => `
+<div class="container" data-aos="fade-up">
+  <h2 class="text-center fs-1 fw-bold">Our Impact</h2>
+
+  <div class="d-flex justify-content-center gap-5">
+      <div class="stats-item py-4 text-center d-flex flex-column gap-1 px-3">
+        <div class="d-flex">
+          <span data-purecounter-start="0" data-purecounter-end="12000" data-purecounter-duration="1" class="purecounter"></span>
+          <p class="fs-6">KG</p>
+        </div>
+        <p>Sampah Plastik</p>
+      </div>
+
+      <div class="stats-item py-4 text-center d-flex flex-column gap-1 px-3">
+        <div class="d-flex">
+          <span data-purecounter-start="0" data-purecounter-end="23000" data-purecounter-duration="1" class="purecounter"></span>
+          <p class="fs-6">KG</p>
+        </div>
+        <p>Limbah</p>
+      </div>
+
+      <div class="stats-item py-4 mb-5 text-center d-flex flex-column gap-1 px-3">
+        <span data-purecounter-start="0" data-purecounter-end="120" data-purecounter-duration="1" class="purecounter"></span>
+        <p>Desa Dibersihkan</p>
+      </div>
+
+      <div  class="stats-item py-4 text-center d-flex flex-column gap-1 px-3">
+        <div class="d-flex">
+          <span data-purecounter-start="0" data-purecounter-end="20" data-purecounter-duration="1" class="purecounter"></span>
+          <p class="fs-6">+</p>
+        </div>
+        <p>Event</p>
+      </div>
+  </div>
+
+</div>
+`
+
+export { createHomePageComponent, CreateCounterComponent };
--- src/scripts/views/pages/homepage.js
+++ src/scripts/views/pages/homepage.js
@@ -1,15 +1,21 @@
-import { createHomePageComponent } from '../components/component-creator';
+import PureCounter from '@srexi/purecounterjs';
+import { CreateCounterComponent, createHomePageComponent } from '../components/component-creator';
 
 const Homepage = {
   async render() {
     return `
       <section id="hero" class="hero"></section>
+      <section id="stats-counter" class="stats-counter"></section>
       `;
   },
 
   async afterRender() {
     const mainContainer = document.querySelector('.hero');
+    const counterContainer = document.querySelector('.stats-counter');
     mainContainer.innerHTML = createHomePageComponent();
+    counterContainer.innerHTML = CreateCounterComponent();
+
+    new PureCounter();
   },
 };
 
