310a2ec9fd306c76b407e89ca820d6b5cb16e6f0
eunoiamo <miftahuda701@gmail.com>
2024-05-29 13:08:23 +0700

    event

--- src/scripts/views/pages/homepage.js
+++ src/scripts/views/pages/homepage.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
 import PureCounter from '@srexi/purecounterjs';
-import { CreateCounterComponent, createHomePageComponent, createVissionMissionComponent } from '../components/component-creator';
+import {
+  CreateCounterComponent, createEventComponent, createHomePageComponent, createVissionMissionComponent,
+} from '../components/component-creator';
 
 const Homepage = {
   async render() {
     return `
       <section id="hero" class="hero"></section>
       <section id="vission" class="vission my-lg-5 pt-lg-4"></section>
+      <section id="future-events" class="py-3 future-events">
+        <div class="container">
+          <h2 class="fs-1 fw-bold text-center my-4" data-aos="fade-up">Our Events</h2>
+          <div class="row gy-4 event-item justify-content-center pb-5 my-4">
+          </div>
+        </div>
+      </section>
       <section id="stats-counter" class="stats-counter"></section>
       `;
   },
@@ -14,9 +25,14 @@ const Homepage = {
     const mainContainer = document.querySelector('.hero');
     const counterContainer = document.querySelector('.stats-counter');
     const vissionContainer = document.querySelector('.vission');
+    const eventContainer = document.querySelector('.event-item');
+
     mainContainer.innerHTML = createHomePageComponent();
     counterContainer.innerHTML = CreateCounterComponent();
     vissionContainer.innerHTML = createVissionMissionComponent();
+    for (let i = 0; i < 3; i++) {
+      eventContainer.innerHTML += createEventComponent();
+    }
 
     new PureCounter();
   },
