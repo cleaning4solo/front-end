c70b9932fee9ad9686c3e62ca0dd795074af24ce
eunoiamo <miftahuda701@gmail.com>
2024-06-01 13:34:46 +0700

    adding scrollup

---
+++ src/scripts/views/pages/detail-blog.js
@@ -0,0 +1,15 @@
+import { } from '../components/component-creator';
+
+const DetailBlog = {
+  async render() {
+    return `
+      <section id="hero" class="hero">DetailBlog Page</section>
+      `;
+  },
+
+  async afterRender() {
+    // code here
+  },
+};
+
+export default DetailBlog;
