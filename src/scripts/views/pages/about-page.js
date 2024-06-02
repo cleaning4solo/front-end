9535d8dda7c32be5442ebd0e5cffce6bf8157da8
eunoiamo <miftahuda701@gmail.com>
2024-06-02 18:55:00 +0700

    updating about page

--- src/scripts/views/pages/about-page.js
+++ src/scripts/views/pages/about-page.js
@@ -1,16 +1,26 @@
-import { createAboutUsComponent } from "../templates/about-us";
-import { galleryIsotope } from "../../components/gallery";
+import PureCounter from '@srexi/purecounterjs';
+import { createAboutUsComponent, createStatsCounterComponent, createTeamComponent } from '../templates/about-us';
+import { galleryIsotope } from '../../components/gallery';
 
 const About = {
   async render() {
     return `
-      <section id="hero" class="hero">About Page</section>
+      <section id="hero" class="hero"></section>
+      <section id="counter" class="counter"></section>
+      <section id="team" class="team"></section>
     `;
   },
 
   async afterRender() {
-    const mainContainer = document.querySelector(".hero");
+    const teamContainer = document.querySelector('.team');
+    teamContainer.innerHTML = createTeamComponent();
+
+    const counterContainer = document.querySelector('.counter');
+    counterContainer.innerHTML = createStatsCounterComponent();
+
+    const mainContainer = document.querySelector('.hero');
     mainContainer.innerHTML = createAboutUsComponent();
+    new PureCounter();
     galleryIsotope();
   },
 };
