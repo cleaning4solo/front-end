4bbf5779e59c5b6ed2d9c279f425693da46b1bcc
LabQii <sisteminformasi2006@gmail.com>
2024-06-13 12:52:18 +0700

    add admin page

--- src/scripts/views/admin-pages/admin.js
+++ src/scripts/views/admin-pages/admin.js
@@ -1,12 +1,25 @@
+import { createSidebarComponent, createContentComponent } from "../templates/template-admin";
+
 const Admin = {
   async render() {
     return `
-      <section id="hero" class="hero">INI ADALAH ADMIN PAGE</section>
+      
+      <section id="sidebar"></section>
+      <section id="content"></section>
     `;
   },
 
+    // <section id="hero" class="hero">INI ADALAH ADMIN PAGE</section>
   // eslint-disable-next-line no-empty-function
-  async afterRender() {},
+  async afterRender() {
+    const sidebarContainer = document.querySelector(".sidebar");
+    sidebarContainer.innerHTML = createSidebarComponent();
+
+    const contentContainer = document.querySelector(".content");
+    contentContainer.innerHTML = createContentComponent();
+
+  },
+  
 };
 
 export default Admin;
